270809ffc548ef691984ce5302762b880102dc52
melou-dev <ameliecuaces.dev@gmail.com>
2019-09-29 12:33:48 +0200

    copy past player1 to player2 into Javascript/script.js of livraison5-score-keeper

--- javascript/script.js
+++ javascript/script.js
@@ -21,11 +21,41 @@ function modifyPlayer1Score() {
     scorePlayer1.innerText = score1;
 }
 
-function GivePointToPlayer1() {
+function givePointToPlayer1() {
     score1++;
     }
 
-    //===============================PLAYER 1 ======================================================
+function globalScore1() {
+    CreatePlayer1Score();
+    modifyPlayer1Score();
+    givePointToPlayer1();
+}
+
+buttonPlayer1.addEventListener(`click`, globalScore1);
+
+
+//===============================PLAYER 2 ======================================================
+
+function CreatePlayer2Score() {
+    scorePlayer2.append(document.createTextNode(score2));
+}
+
+function modifyPlayer2Score() {
+    scorePlayer2.innerText = score2;
+}
+
+function givePointToPlayer2() {
+    score2++;
+    }
+
+function globalScore2() {
+    CreatePlayer2Score();
+    modifyPlayer2Score();
+    givePointToPlayer2();
+}
+
+buttonPlayer2.addEventListener(`click`, globalScore2);
+
 
 //function modifyPlayer1Score() {}
 
